f1c6f49085ad661498dffdab016b0b8872ac9fa0
Kyara Esteves de Sousa <00334681170@ifb.local>
2026-06-16 17:16:06 -0300

    exercício 1 feito

---
+++ ex1.js
@@ -0,0 +1,11 @@
+const num1 = 20;
+const num2 = 4;
+const soma = num1 + num2;
+const subtracao = num1 - num2;
+const multiplicacao = num1 * num2;
+const divisao = num1 / num2;
+console.log(`Soma: ${num1} + ${num2} = ${soma}`);
+console.log(`Subtração: ${num1} - ${num2} = ${subtracao}`);
+2 / 4
+console.log(`Multiplicação: ${num1} * ${num2} = ${multiplicacao}`);
+console.log(`Divisão: ${num1} / ${num2} = ${divisao}`);
